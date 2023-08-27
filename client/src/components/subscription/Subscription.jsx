4ca2c9087218324f455733608a241c145168ea09
Divvi satya sriram <111105934+Sriram-99@users.noreply.github.com>
2023-08-28 04:47:39 +0530

    Update Subscription.jsx

--- client/src/components/subscription/Subscription.jsx
+++ client/src/components/subscription/Subscription.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import './Subscription.css';
+
 import Paybutton from './Paybutton'
 import axios from 'axios';
 import proxy from '../../actions/proxy';
